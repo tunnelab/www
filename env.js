7339a82489d021b003fce09a698f9069057c8d51
tunnelab <tunnelab@protonmail.com>
2020-06-16 21:53:10 +0800

    init

---
+++ env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'Tunnel lab',
+  // 站点描述
+  description: '隧道实验室 🧪',
+  // API
+  host: 'https://api.tunnelab.com',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://images.unsplash.com/photo-1530212486532-d957d9c01d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3367&q=80'
+}
